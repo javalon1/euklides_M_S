fc1eb1147345607fc80fb551cc975147b22541bc
Michal Stusik <x5htpx@gmail.com>
2019-05-12 12:49:03 +0100

    NWD

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,14 +1,47 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+   /* public static int euklides (){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        while (A=B){
+             System.out.println(A);
+
+
+
+
+        }
+
+}*/
+
+
     public static void main( String[] args )
 
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int licznik =0;
+
+        while (a!=b){
+            if (a>b){
+                a=a-b;
+            }else{
+                b=b-a;
+            }
+            licznik++;
+        }
+        System.out.println("liczba interakcji: " + licznik);
+        System.out.println("NWD: " + a);
+
+
     }
 }
